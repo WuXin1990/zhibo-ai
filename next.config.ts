23b9ab29085ebb924231341ab11491a007259fb5
WuXin1990 <947909990@qq.com>
2025-12-03 10:36:45 +0800

    添加eslintignore忽略文件

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. TypeScript 检查通常还可以保留（如果这里也报错，就把这块也删了）
+  // ✅ 只保留这个，忽略 TS 错误
   typescript: {
     ignoreBuildErrors: true,
   },
   
-  // 2. 图片跨域配置 (保留)
   images: {
     remotePatterns: [
       {
